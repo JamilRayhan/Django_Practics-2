80cc6b7b8b5a169e23ce493fa6cc269064d859a0
JamilRayhan <jamilrayhan.cse@gmail.com>
2023-03-06 01:05:58 +0600

    auth

--- login_app/urls.py
+++ login_app/urls.py
@@ -6,5 +6,8 @@ from django.conf.urls.static import static
 app_name='login_app'
 
 urlpatterns = [
-    path('',views.index,name='index')
+    path('',views.index,name='index'),
+    path('register/',views.register,name='register'),
+    path('login/',views.login_page,name='login'),
+    path('user_login/',views.user_login,name='user_login')
 ]
